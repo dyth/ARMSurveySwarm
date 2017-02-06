33b03a9759b463bfc4336be10bfc20779a3483d8
jamienet <Jamie@jd726a.trin.private.cam.ac.uk>
2017-02-06 15:58:20 +0000

    Added code to draw image to canvas

---
+++ Webapp/javascript/draw.js
@@ -0,0 +1,70 @@
+/**
+ * Created by Jamie on 06/02/2017.
+ */
+
+var canvas;
+var stage;
+var graph;
+
+// TODO: Replace with actual tile data
+var tiles = [
+    [1,0,0,1,0,1,0],
+    [0,1,0,0,0,1,1],
+    [1,1,0,1,1,1,0],
+    [0,1,1,0,0,0,0],
+    [1,0,1,0,1,1,0],
+    [0,1,1,0,0,1,1],
+    [0,0,1,0,0,1,0]
+];
+
+$(function () {
+
+    // Get references to the canvas and a new stage
+    canvas = $("#canvas");
+    stage = new createjs.Stage("canvas");
+
+    // Resize the canvas to fill the entire width
+    resizeCanvas();
+
+    // Create a new graph shape
+    graph = new createjs.Shape();
+
+    // Add the graph to the stage
+    stage.addChild(graph);
+
+});
+
+function resizeCanvas(){
+
+    var size = canvas.parent().width();
+
+    canvas.width(size);
+    canvas.height(size);
+
+}
+
+function updateCanvas() {
+
+    // Clear the stage
+    stage.clear();
+
+    // Calculate square size
+    var size = canvas.width() / tiles.length;
+
+    // Draw squares
+    for(var i = 0; i<tiles.length; i++){
+
+        for(var j = 0; j<tiles.length; j++){
+
+            if(tiles[i][j] == 1)
+                graph.graphics.beginFill("#000").drawRect(j*size, i*size, size, size);
+            else
+                graph.graphics.beginFill("#fff").drawRect(j*size, i*size, size, size);
+
+        }
+
+    }
+
+    stage.update();
+
+}
