657571676e85757940be8b5d793752931695eb93
Jackson Woodruff <woodruff.jackson@gmail.com>
2017-02-09 12:27:58 +0000

    Added some initial tests

--- Webapp/backend/test/processing_test.js
+++ Webapp/backend/test/processing_test.js
@@ -1,9 +1,14 @@
+var processor = require('../processing');
+
 var assert = require('assert');
 
 describe('Create tiles list', function() {
+	it('should print "test"', function() {
+		processor.test();
+	});
 	it('should return a new list of tiles of size 10, 10', function() {
-		var tiles = createTilesList(10, 10);
-		assert.equal(10, tiles.length);
-		assert.equal(10, tiles[0].length);
+		processor.test(10, 10);
+		assert.equal(10, processor.processingTiles.length);
+		assert.equal(10, processor.processingTiles.length);
 	});
 });
