69529bab4a96e2f1dbe1cd4d6fe8dd42293df149
Jackson Woodruff <woodruff.jackson@gmail.com>
2017-02-12 17:00:20 +0000

    Added some more tests to server_test

--- Webapp/backend/test/server_test.js
+++ Webapp/backend/test/server_test.js
@@ -11,6 +11,139 @@ var options = {
 	'force new connection': true
 }
 
+describe('calling updateTile', function() {
+	it('should trigger an update for the sendTileData function ' +
+		'without reordeirng data', function(done) {
+		var client = io.connect(socketURL, options);
+		var tileData = [];
+
+		client.on('connect', function() {
+			server.updateTile(10, 10, 3);
+			server.updateTile(10, 10, 1);
+			server.updateTile(5, 1, 0);
+		});
+
+		client.on('sendTileData', function(data) {
+			tileData.push(data);
+
+			if (tileData.length === 3) {
+				done();
+			}
+		});
+
+		setTimeout(function() {
+			expect(tileData.length).to.equal(3);
+			expect(tileData[0].x).to.equal(10);
+			expect(tileData[0].y).to.equal(10);
+			expect(tileData[0].value).to.equal(3);
+			expect(tileData[1].value).to.equal(1);
+			expect(tileData[2].value).to.equal(0);
+		}, 300);
+	});
+});
+describe('Calling updateStatus', function() {
+	it('should trigger a broadcast with the robot status', function(done) {
+		var clients = [];
+		var receivedData1 = [];
+		var receivedData2 = [];
+		var NUM = 50;
+		var connections = 0;
+
+		// initialize all the clients.
+		for (var i = 0; i < NUM; i ++) {
+			var newClient = io.connect(socketURL, options);
+
+			newClient.on('sendRobotStatus', function(data) {
+				if (data.id === 1) {
+					receivedData1.push(data);
+				} else {
+					receivedData2.push(data);
+				}
+
+				if (receivedData1.length === NUM 
+					&& receivedData2.length === NUM) {
+					done();
+				}
+			});
+
+			newClient.on('connect', function() {
+				connections ++;
+
+				if (connections == NUM) {
+					// If all the servers are connected, then
+					// run the broadcasts.
+					server.updateStatus(1, 10, 5, 'running');
+					server.updateStatus(3, 2, 1, 'stopped');
+
+				}
+			});
+
+			clients.push(newClient);
+		}
+
+		setTimeout(function() {
+			expect(receivedData1.length, "not all clients received" + 
+			" message 1: " + receivedData2.length.toString ()+ "/" 
+				+ NUM.toString() + "\n").to.equal(NUM);
+			expect(receivedData2.length, "not all clients received" + 
+			" message 2: " + receivedData2.length.toString() + "/" + 
+				NUM.toString() + "\n").to.equal(NUM);
+
+			for (var i = 0; i < receivedData1.length; i ++) {
+				expect(receivedData1[i].id).to.equal(1);
+				expect(receivedData1[i].x).to.equal(10);
+				expect(receivedData1[i].y).to.equal(5);
+				expect(receivedData1[i].status).to.equal('running');
+			}
+
+			for (var i = 0; i < receivedData2.length; i ++) {
+				expect(receivedData2[i].id).to.equal(3);
+				expect(receivedData2[i].x).to.equal(2);
+				expect(receivedData2[i].y).to.equal(1);
+				expect(receivedData2[i].status).to.equal('stopped');
+			}
+		}, 600);
+	});
+});
+
+describe('Calling updateGrid', function() {
+	it('should trigger a boradcast with new area dimensions', function(done) {
+		var client1 = io.connect(socketURL,  options);
+		var client2 = io.connect(socketURL,  options);
+
+		var receivedData = [];
+		var client1Received = false;
+		var client2Received = false;
+		client1.on('sendAreaDimensions', function(data) {
+			receivedData.push(data);
+			client1Received = true;
+			if (client1Received && client2Received) {
+				done();
+			}
+		});
+
+		client2.on('sendAreaDimensions', function(data) {
+			receivedData.push(data);
+			client2Received = true;
+			if (client1Received && client2Received) {
+				done();
+			}
+		});
+
+		server.updateGrid(10, 20);
+
+		setTimeout(function() {
+			expect(receivedData[0].xDim, 'x data').to.equal(10);
+			expect(receivedData[0].yDim, 'y data').to.equal(20);
+			expect(receivedData[1].xDim, 'x data').to.equal(10);
+			expect(receivedData[1].yDim, 'y data').to.equal(20);
+
+			client1.disconnect();
+			client2.disconnect();
+		}, 1000);
+	});
+});
+
 describe('Stop Function Test', function() {
 	it('should return an ack by emitting "stopCalled"', function(done) {
 		var client = io.connect(socketURL, options);
