6abb01967324aa3586e0d6548e31c3dc0a95e452
David Hui <dyth@users.noreply.github.com>
2017-02-14 12:53:37 +0000

    Update reflectance.cpp

--- 3pi/reflectance.cpp
+++ 3pi/reflectance.cpp
@@ -20,28 +20,28 @@ void TurnClockwise(int degree) {
 
 void goForwards(int distance) {
         // goes forward distance mm
-        m3pi.forwards(0.5);
+        m3pi.forward(0.5);
         wait (distance / 470.0);
         m3pi.stop();
     }
     
 void goBackwards(int distance) {
         // goes backwards distance mm
-        m3pi.backwards(0.5);
+        m3pi.backward(0.5);
         wait (distance / 470.0);
         m3pi.stop();
     }
 
+
 int main() {
-    
+    wait(0.2);
     m3pi.sensor_auto_calibrate();
-    wait(0.5);
-    
+    wait(0.2);
     while(1) {
         m3pi.locate(0,1);
         float line = m3pi.line_position();
         m3pi.printf("%f", line);
         wait(0.1);
         m3pi.cls();
-    }  
+    }
 }
