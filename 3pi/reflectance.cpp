d09092d602242e8219e189cde61b8f9556597689
David Hui <dyth@users.noreply.github.com>
2017-02-25 18:24:01 +0000

    fixed indentation

--- 3pi/reflectance.cpp
+++ 3pi/reflectance.cpp
@@ -4,34 +4,33 @@
 
 m3pi m3pi;
 
-void TurnCounterClockwise(int degree) {
-        // Turn left at half speed
-        m3pi.left(0.5);
-        wait (degree * 0.5538461538461539 / 360.0);
-        m3pi.stop();
-    }
+void turnCounterClockwise(int degree) {
+    // Turn left at half speed
+    m3pi.left(0.5);
+    wait (degree * 0.554 / 360.0);
+    m3pi.stop();
+}
     
-void TurnClockwise(int degree) {
-        // Turn right at half speed
-        m3pi.right(0.5);
-        wait (degree * 0.5538461538461539 / 360.0);
-        m3pi.stop();
-    }
+void turnClockwise(int degree) {
+    // Turn right at half speed
+    m3pi.right(0.5);
+    wait (degree * 0.554 / 360.0);
+    m3pi.stop();
+}
 
 void goForwards(int distance) {
-        // goes forward distance mm
-        m3pi.forward(0.5);
-        wait (distance / 470.0);
-        m3pi.stop();
-    }
+    // goes forward distance mm
+    m3pi.forward(0.5);
+    wait (distance / 470.0);
+    m3pi.stop();
+}
     
 void goBackwards(int distance) {
-        // goes backwards distance mm
-        m3pi.backward(0.5);
-        wait (distance / 470.0);
-        m3pi.stop();
-    }
-
+    // goes backwards distance mm
+    m3pi.backward(0.5);
+    wait (distance / 470.0);
+    m3pi.stop();
+}
 
 int main() {
     wait(0.2);
