ef73db6abdb3be694a397c9a444519d4c9347d3b
Jackson Woodruff <woodruff.jackson@gmail.com>
2017-03-01 13:26:59 +0000

    Only one robot at a time

--- Webapp/backend/robot_sim/Test.java
+++ Webapp/backend/robot_sim/Test.java
@@ -5,32 +5,7 @@ class Test {
 		{0, 0, 1, 1} };
 
 	public static void main(String[] args) {
-		Thread thread = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				Robot r = new Robot(0, tiles, 10);
-				r.connect("localhost");
-			}
-		});
-
-		Thread thread2 = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				Robot r = new Robot(1, tiles, 10);
-				r.connect("localhost");
-			}
-		});
-
-		Thread thread3 = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				Robot r = new Robot(2, tiles, 10);
-				r.connect("localhost");
-			}
-		});
-
-		thread.start();
-		thread2.start();
-		thread3.start();
+		Robot r = new Robot(0, tiles, 10);
+		r.connect("localhost");
 	}
 }
