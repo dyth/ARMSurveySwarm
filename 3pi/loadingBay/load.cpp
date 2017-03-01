a10f79d50d80b99fdd2cf2a2766f1c2433e4bef6
David Hui <dyth@users.noreply.github.com>
2017-03-01 02:19:52 +0000

    now with debouncing

--- 3pi/loadingBay/load.cpp
+++ 3pi/loadingBay/load.cpp
@@ -1,24 +1,48 @@
 #include "mbed.h"
 #include "m3pi.h"
+#include <algorithm>
  
 m3pi m3pi;
- 
+
+// Debouncing speed
+#define DEBOUNCE 1
+
 // Minimum and maximum motor speeds
 #define MAX 1.0
-#define MIN 0
+#define MIN 0.0
  
 // PID terms
 #define P_TERM 1
 #define I_TERM 0
 #define D_TERM 20
- 
+
+float sum (float* rotations) {
+    // sum up debounce array
+    float s = 0.0f;
+    for (int j = 0; j < DEBOUNCE; j++) {
+        s += rotations[j];
+    }
+    return s;
+}
+
+float limit(float speed) {
+    // ensures speed is between MIN amd MAX
+    if (speed < MIN) {
+        return MIN;
+    } else if (speed > MAX) {
+        return MAX;
+    } else {
+        return speed;
+    }
+}
+
 int main() {
- 
-    m3pi.locate(0,1);
-    m3pi.printf("Line PID");
+    
+    m3pi.cls();
+    m3pi.locate(0,0);
+    m3pi.printf("PID");
  
     wait(0.5);
- 
     m3pi.sensor_auto_calibrate();
  
     float right;
@@ -29,57 +53,46 @@ int main() {
     float power;
     float speed = MAX;
     
+    // create array for debouncing and fill it with null values
+    float rotations[DEBOUNCE];
+    std::fill(rotations, rotations + DEBOUNCE, 1.0f);
     int count = 0;
-     
-    while (1) {
-        count++;
- 
+    
+    float s = 0.0f;
+    
+    while (s != 0.0f) {
         // Get the position of the line.
         current_pos_of_line = m3pi.line_position();        
         proportional = current_pos_of_line;
         
-        // Compute the derivative
+        // Compute the derivative, integral, remember previous position
         derivative = current_pos_of_line - previous_pos_of_line;
-        
-        // Compute the integral
         integral += proportional;
-        
-        // Remember the last position.
         previous_pos_of_line = current_pos_of_line;
         
-        // Compute the power
-        power = (proportional * (P_TERM) ) + (integral*(I_TERM)) + (derivative*(D_TERM)) ;
-        
-        // Compute new speeds   
+        // Compute the power and use it to find new speeds
+        power = (proportional * (P_TERM) ) + (integral*(I_TERM)) + (derivative*(D_TERM));
         right = speed+power;
         left  = speed-power;
         
-        // limit checks
-        if (right < MIN) {
-            right = MIN;
-        } else if (right > MAX) {
-            right = MAX;
-        }
-            
-        if (left < MIN) {
-            left = MIN;
-        } else if (left > MAX) {
-            left = MAX;
-        }
+        // set speed at limits
+        left = limit(left);
+        right = limit(right);
+        m3pi.left_motor(left/2);
+        m3pi.right_motor(right/2);
         
-        if (count > 20 && (right == MIN || left == MIN)) {
-            break;
+        // do some debouncing
+        rotations[count++] = left;
+        if (count == DEBOUNCE) {
+            count = 0;
         }
+        s = sum(rotations);
         
-        // set speed 
-        m3pi.left_motor(left / 2);
-        m3pi.right_motor(right / 2);
- 
+        m3pi.cls();
+        m3pi.locate(0,0);
+        //m3pi.printf("%.5s", sum);
+        //wait(0.001);
+        
     }
     m3pi.stop();
-    m3pi.cls();
-    m3pi.locate(0,0);
-    m3pi.printf("%f", left);
-    m3pi.locate(0,1);
-    m3pi.printf("%f", right);
 }
