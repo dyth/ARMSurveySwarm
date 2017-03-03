2d9606d7b6ca7b22c964e0befd3b37697f025622
David Hui <dyth@users.noreply.github.com>
2017-03-03 18:31:16 +0000

    major cleanup of code

---
+++ 3pi/counterClockwise.cpp
@@ -0,0 +1,294 @@
+#include "mbed.h"
+#include "m3pi.h"
+#include <algorithm>
+#include <sstream>
+ 
+// PID terms
+#define P_TERM 1
+#define I_TERM 0
+#define D_TERM 20
+
+// board size
+#define tileSize 100
+
+// robot terms
+#define clockwiseRotation 2.483f
+#define counterRotation 2.235f
+#define robotMotorLeft 0.5f
+#define robotMotorRight 0.5f
+#define robotDistancePerSecond 470.0f
+
+m3pi m3pi;
+
+void halt() {
+    // halt robot and allow motors to cool down
+    m3pi.stop();
+    wait(1.0f);   
+}
+
+void turnCounterClockwise(int degree) {
+    // Turn left at the slowest speed possible for accuracy
+    m3pi.left(0.15f);
+    wait(((float) degree) * counterRotation / 360.0f);
+    halt();
+}
+
+void turnClockwise(int degree) {
+    // Turn right at the slowset speed possible for accuracy
+    m3pi.stop();
+    m3pi.right(0.15f);
+    wait(((float) degree) * clockwiseRotation / 360.0f);
+    halt();
+}
+
+void cadence() {
+    // drive straight for 1 second whilst sampling twice per tile size
+    
+    // define constants and varibles for sampling
+    int samples = (int) floor(robotDistancePerSecond / ((float) tileSize / 2.0f));
+    int intensities[samples];
+    
+    // move forward for remainder
+    m3pi.left_motor(robotMotorLeft);
+    m3pi.right_motor(robotMotorRight);
+    wait((int) robotDistancePerSecond % (int) (tileSize / 2) / robotDistancePerSecond);
+    
+    // sample twice per tile size
+    int sensors[5];
+    for (int i = 0; i < samples; i++) {
+        m3pi.calibrated_sensor(sensors);
+        wait(1.0f / (float) samples);
+        intensities[i] = sensors[2];
+    }
+    halt();
+}
+
+void anneal() {
+    // anneal movement for 0.25 second
+    m3pi.forward(0.25);
+    wait(0.25);
+    m3pi.backward(0.25);
+    wait(0.25);
+    halt();
+}
+
+void goForwards(int distance) {
+    // go forwards in cadences of 1 second of bleed move and anneal
+    m3pi.stop();
+    
+    //bleed
+    m3pi.forward(0.25);
+    wait(0.25);
+    
+    //move
+    while (distance > robotDistancePerSecond) {
+        cadence();
+        distance -= robotDistancePerSecond;
+        //anneal();
+    }
+    
+    // move remainder of distance
+    m3pi.left_motor(robotMotorLeft);
+    m3pi.right_motor(robotMotorRight);
+    wait(((float) distance) / robotDistancePerSecond);
+    halt();
+    
+    //anneal();
+}
+
+float sum (float* rotations, int debounce) {
+    // return sum of debounce array
+    float s = 0.0f;
+    for (int j = 0; j < debounce; j++) {
+        s += rotations[j];
+    }
+    return s;
+}
+
+float limit(float speed, float MIN, float MAX) {
+    // ensures MIN < speed < MAX
+    if (speed < MIN)
+        return MIN;
+    else if (speed > MAX)
+        return MAX;
+    else
+        return speed;
+}
+
+void PIDFast() {
+    // PID line following between the speeds MIN and MAX
+    float MIN = 0.0f;
+    float MAX = 1.0f;
+    float rightTotal;
+    float leftTotal;
+    float current_pos_of_line = 0.0;
+    float previous_pos_of_line = 0.0;
+    float derivative, proportional, integral = 0;
+    float speed = MAX;
+    int count = 0;
+    
+    // loop until debouncing has succeeded
+    while (count++ < 500) {
+        // Get the position of the line.
+        current_pos_of_line = m3pi.line_position();        
+        proportional = current_pos_of_line;
+        
+        // Compute the derivative, integral, remember previous position
+        derivative = current_pos_of_line - previous_pos_of_line;
+        integral += proportional;
+        previous_pos_of_line = current_pos_of_line;
+        
+        // Compute the power and use it to find new speeds
+        float power = (proportional * (P_TERM) ) + (integral*(I_TERM)) + (derivative*(D_TERM));
+        float right = speed+power;
+        float left  = speed-power;
+        
+        // set speed at limits
+        left = limit(left, MIN, MAX);
+        right = limit(right, MIN, MAX);
+        m3pi.left_motor(left);
+        m3pi.right_motor(right);
+        
+        leftTotal += left;
+        rightTotal += right;
+    }
+    // stop and calibrate sensors
+    halt();
+}
+    
+void PID(float MIN, float MAX, int debounce) {
+    // PID line following between the speeds MIN and MAX
+    float rightTotal;
+    float leftTotal;
+    float current_pos_of_line = 0.0;
+    float previous_pos_of_line = 0.0;
+    float derivative, proportional, integral = 0;
+    float speed = MAX;
+    int in = 0;
+    
+    // create array for debouncing and fill it with a starting
+    float rotations[debounce];
+    std::fill(rotations, rotations + debounce, 1.0f);
+    int count = 0;
+    
+    float s = 1.0f;
+    
+    // loop until debouncing has succeeded
+    while (s != 0.0f) {
+        in++;
+        // Get the position of the line.
+        current_pos_of_line = m3pi.line_position();        
+        proportional = current_pos_of_line;
+        
+        // Compute the derivative, integral, remember previous position
+        derivative = current_pos_of_line - previous_pos_of_line;
+        integral += proportional;
+        previous_pos_of_line = current_pos_of_line;
+        
+        // Compute the power and use it to find new speeds
+        float power = (proportional * (P_TERM) ) + (integral*(I_TERM)) + (derivative*(D_TERM));
+        float right = speed+power;
+        float left  = speed-power;
+        
+        // set speed at limits
+        left = limit(left, MIN, MAX);
+        right = limit(right, MIN, MAX);
+        m3pi.left_motor(left);
+        m3pi.right_motor(right);
+        
+        leftTotal += left;
+        rightTotal += right;
+        
+        // do some debouncing
+        if (in > 50) {
+            rotations[count++] = left;
+            if (count == debounce) {
+                count = 0;
+            }
+            s = sum(rotations, debounce);
+        }
+    }
+    // stop and calibrate sensors
+    halt();
+}
+
+void alignCorner() {
+    // aligns a robot such that it is on the corner, facing the new direction
+    
+    // find corner quickly, then align with corner, reverse and then
+    // slowly level up until corner is detected
+    
+    //turn to new direction (perpendicular to the starting position)
+    PIDFast();
+    PID(0.0, 0.25, 2);
+    
+    turnClockwise(90);
+}
+
+void findLine(float speed) {
+    // go forwards until line is found
+    
+    int sensors[5];
+    
+    while(1) {
+        m3pi.forward(0.15);
+        m3pi.calibrated_sensor(sensors);
+    
+        // if black, advance length of tile
+        // if no longer black, then tile detected, keep on advancing
+        // otherwise, edge detected, turn to face new corner
+        if (sensors[2] > 900)  {
+            wait(0.4f);
+            //break;
+            m3pi.calibrated_sensor(sensors);
+            if (sensors[2] < 900) {
+                halt();
+                turnClockwise(90);
+                break;
+            } else {
+                wait(0.85f);
+            }
+        }
+    }
+    
+}
+
+void cycleClockwise(double degree, double distance) {
+    // go to point (x, y), then find the edge, then find the next corner
+    
+    // go to point (degree, distance) then face the edge
+    turnClockwise((float) degree);
+    goForwards((double) distance);
+    turnClockwise((float) 270.0f - degree);
+    
+    //goForwards((distance * sin(degree * 3.141592654f / 180.0f)) - 200.0f);
+        
+    // go forwards until edge detected
+    findLine(0.5f);
+    
+    // recalibrate and align with corner
+    m3pi.sensor_auto_calibrate();
+    alignCorner();
+}
+
+
+int main() {
+    // wait until human has left then autocalibrate
+    m3pi.reset();
+    wait(0.5);
+    
+    m3pi.sensor_auto_calibrate();
+    alignCorner();
+    
+    // main loop of program
+    while (1) {
+        int x = 800;
+        int y = 500;
+        
+        double distance = (double) pow(x, 2.0f) + pow(y, 2.0f);
+        double travel = (double) sqrt(distance);
+        float degree = atan2 ((float) y, (float) x) * 180.0f / 3.141592654f;
+        
+        cycleClockwise(degree, travel);
+    }
+}
