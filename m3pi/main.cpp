9232ecab84d9ec239a2bccee60ca6cf9f97a8a30
Jamie Davenport <jamiedav97@gmail.com>
2017-03-05 01:42:19 +0000

    Update main.cpp

--- m3pi/main.cpp
+++ m3pi/main.cpp
@@ -8,16 +8,36 @@ ESP8266Interface wifi(p28, p27);
 SocketAddress server(SERVIP, SERVPORT);
 TCPSocket socket;
 
-void readAndDecodeInstruction(MbedJSONValue &instruction){
+string vectorToString(vector<int> &v){
 
+    ostringstream ss;
+
+    ss << "[";
+
+    for(int i = 0; i < v.size(); i++){
+
+        ss << i;
+        if(i < v.size() - 1)
+            ss << ",";
+
+    }
+
+    ss << "]";
+
+    return ss.str();
+
+}
+
+void readAndDecodeInstruction(MbedJSONValue &instruction){
+    
     // Reads instruction
     char rbuffer[64];
     int rcount = socket.recv(rbuffer, sizeof rbuffer);
     pc.printf("\n**** NEW INSTRUCTION ****\n%s\n", rbuffer);
-
+    
     // Decode instruction
     parse(instruction, rbuffer);
-
+    
 }
 
 /*
@@ -25,12 +45,12 @@ void readAndDecodeInstruction(MbedJSONValue &instruction){
 * Robot gets the tile size in mm.
 */
 void handleStart(MbedJSONValue &instruction){
-
+    
     // Get the tile size
     //int tileSize = instruction["tileSize"].get<int>();
-
+    
     pc.printf("START(%d)\n", tileSize);
-
+    
 }
 
 /*
@@ -38,29 +58,29 @@ void handleStart(MbedJSONValue &instruction){
 * Robot gets the angle in degrees and distance in mm.
 */
 void handleMove(MbedJSONValue &instruction, vector<int> &intensities){
-
+    
     // Reset the intensities vector
     intensities.clear();
-
+    
     // Get the angle and distance
     int angle = instruction["angle"].get<int>();
     int distance = instruction["distance"].get<int>();
-
+    
     pc.printf("MOVE(%d, %d)\n", angle, distance);
-
+    
     cycleClockwise(angle, distance, intensities);
-
+    
     sendDone(intensities);
-
+    
 }
 
 /*
 * Called when a STOP instruction is received.
 */
 void handleStop(MbedJSONValue &instruction){
-
+    
     pc.printf("STOP\n");
-
+    
 }
 
 /*
@@ -68,12 +88,12 @@ void handleStop(MbedJSONValue &instruction){
 * Robot gets the time to wait in ms
 */
 void handleWait(MbedJSONValue &instruction){
-
+    
     // Get the time to wait
     int time = instruction["time"].get<int>();
-
+    
     pc.printf("WAIT(%d)\n", time);
-
+    
 }
 
 /*
@@ -86,41 +106,38 @@ void sendHello(){
     MbedJSONValue message;
     message["type"] = "HELLO";
     message["id"] = 0; // TODO: CHANGE THIS
-
+    
     // Serialize the message
     string toSend = message.serialize();
-
+    
     // Send the message
     socket.send(toSend.c_str(), toSend.size());
-
+    
     pc.printf("SENDING(%s)\n", toSend);
 
 }
 
 /*
-*
-*
+* 
+* 
 */
 void sendDone(vector<int> &intensities){
-
+    
     // Create the DONE message
     MbedJSONValue message;
     message["type"] = "DONE";
-
-    // Add the intensities
-    // TODO: LOOK INTO SENDING MORE THAN 20 THINGS
-    for(int i = 0; i < intensities.size(); i++){
-        message["intensities"][i] = intensities[i];
-    }
-
+    
+    string s = vectorToString(intensities);
+    message["intensities"] = s;
+    
     // Serialize the message
     string toSend = message.serialize();
-
+    
     // Send the message
     socket.send(toSend.c_str(), toSend.size());
-
+    
     pc.printf("SENDING(%s, %d)\n", toSend, intensities.size());
-
+    
 }
 
 /*
@@ -128,35 +145,35 @@ void sendDone(vector<int> &intensities){
 * Continuously reads instructions and processes them
 */
 void run(){
-
+    
     MbedJSONValue instruction;
-
+    
     // Create a vector to hold intensities
     vector<int> intensities;
-
+    
     while(1){
-
+        
         // Get the instruction
         readAndDecodeInstruction(instruction);
-
+        
         // Handle instruction
         string type = instruction["type"].get<string>();
-
+        
         if(type.compare("START") == 0) handleStart(instruction);
-
+        
         if(type.compare("MOVE") == 0) handleMove(instruction, intensities);
-
+        
         if(type.compare("WAIT") == 0) handleWait(instruction);
-
+        
         if(type.compare("STOP") == 0) handleStop(instruction);
-
+    
     }
-
-
+    
+    
 }
 
 int main() {
-
+    
     printf("\n\n******************************\n");
     printf("ESP8266 WiFi control\n");
 
@@ -174,8 +191,6 @@ int main() {
     socket.open(&wifi);
     socket.connect(server);
 
-    pc.printf("Connected\n");
-
     // Setup robot
     start();
 
@@ -191,6 +206,6 @@ int main() {
     // Bring down the ESP8266 WiFi connection
     wifi.disconnect();
     printf("Done\n");
-
-
+    
+    
 }
