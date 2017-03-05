fad4160df0f8091a23538d3866915f3fa0564661
Jamie Davenport <jamiedav97@gmail.com>
2017-03-05 03:51:56 +0000

    Update main.h

--- m3pi/main.h
+++ m3pi/main.h
@@ -4,14 +4,12 @@
 #include "ESP8266Interface.h"
 #include "SocketAddress.h"
 #include <string>
-#include <sstream>
 
 #define SERVIP "192.168.46.4"
 #define SERVPORT 9000
 #define SSID "private_network152"
 #define PASSWORD "CelesteAqua78"
 
-string vectorToString(vector<int> &v);
 void readAndDecodeInstruction(MbedJSONValue &instruction);
 void sendDone(vector<int> &intensities);
 void handleStart(MbedJSONValue &instruction);
