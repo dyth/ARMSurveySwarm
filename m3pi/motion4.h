2939bef9c3668f089d9842a75e33d87227da37cf
David Hui <dyth@users.noreply.github.com>
2017-03-06 10:43:26 +0000

    Create motion4.h

---
+++ m3pi/motion4.h
@@ -0,0 +1,21 @@
+#include "mbed.h"
+#include "m3pi.h"
+#include <algorithm>
+#include <iterator>
+#include <vector>
+
+// robot terms
+#define rotationSpeed 0.5f
+#define robotID 0
+#define rotation 0.674f
+#define robotMotorLeft 0.5f
+#define robotMotorRight 0.5734f
+#define robotDistancePerSecond 440.0f
+#define robotTurningCorrection 85
+#define BLEED true
+
+void start();
+void alignCorner(int distance);
+void alignCorner();
+void cycleClockwise(int degree, int distance, vector<int> &vectorIntensities);
+void setTileSize(int size);
