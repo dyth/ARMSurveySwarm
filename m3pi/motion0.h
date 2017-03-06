c3fd9cccb3347552a4c9e8d7e19298a7066a0efa
Lucia Bura <luciabura@gmail.com>
2017-03-06 11:59:51 +0000

    Added motion0.h

---
+++ m3pi/motion0.h
@@ -0,0 +1,21 @@
+#include "mbed.h"
+#include "m3pi.h"
+#include <algorithm>
+#include <iterator>
+#include <vector>
+
+// robot terms
+#define robotID 1
+#define rotation 0.498f
+#define rotationSpeed 0.5f
+#define robotMotorLeft 0.500f
+#define robotMotorRight 0.5028f
+#define robotDistancePerSecond 470.0f
+#define robotTurningCorrection 75
+#define BLEED true
+
+void start();
+void alignCorner(int distance);
+void alignCorner();
+void cycleClockwise(int degree, int distance, vector<int> &vectorIntensities);
+void setTileSize(int size);
