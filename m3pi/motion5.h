f79e815ef5cf3dd1975bdf32939f3c753a740c8e
hrutvik <hrutvikkanabar@yahoo.co.uk>
2017-03-07 11:14:05 +0000

    Update motion5.h

--- m3pi/motion5.h
+++ m3pi/motion5.h
@@ -1,1 +1,21 @@
-//
+#include "mbed.h"
+#include "m3pi.h"
+#include <algorithm>
+#include <iterator>
+#include <vector>
+
+// robot terms
+#define rotationSpeed 0.5f
+#define robotID 0
+#define rotation 0.697f
+#define robotMotorLeft 0.5f
+#define robotMotorRight 0.5f
+#define robotDistancePerSecond 5.20f
+#define robotTurningCorrection 82
+#define BLEED true
+
+void start();
+void alignCorner(int distance);
+void alignCorner();
+void cycleClockwise(int degree, int distance, vector<int> &vectorIntensities);
+void setTileSize(int size);
