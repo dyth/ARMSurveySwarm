3c7e0e50d2c14ddc48aa5c7efcbd07db721280e4
Jackson Woodruff <woodruff.jackson@gmail.com>
2017-03-08 10:40:54 +0000

    Update motion2.h

--- m3pi/motion2.h
+++ m3pi/motion2.h
@@ -7,10 +7,10 @@
 // robot terms
 #define rotationSpeed 0.5f
 #define robotID 2
-#define rotation 0.684f
-#define robotMotorLeft 0.5f
-#define robotMotorRight 0.5125f
-#define robotDistancePerSecond 420.0f
+#define rotation 0.551f
+#define robotMotorLeft 0.502f
+#define robotMotorRight 0.50f
+#define robotDistancePerSecond 485.0f
 #define robotTurningCorrection 85
 #define BLEED true
 
